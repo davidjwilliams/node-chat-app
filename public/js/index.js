034040e67a8c323f153a0fd2d081a995443b83fb
davidjwilliams <davidj_williams@hotmail.com>
2016-12-27 17:00:56 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function(){
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'David',
-		text: 'Hey. This is David.'
-	});
 });
 
 socket.on('disconnect', function() {
