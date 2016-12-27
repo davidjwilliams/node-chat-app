99447f513684bbdd77900249bf3b2a7e1b226c03
davidjwilliams <davidj_williams@hotmail.com>
2016-12-27 20:30:45 +0000

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -15,19 +15,48 @@ socket.on('newMessage', function(message){
 	jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-	from: 'David',
-	text: 'Hi'
-}, function(data){
-	console.log('Got it!', data);
+// socket.emit('createMessage', {
+// 	from: 'David',
+// 	text: 'Hi'
+// }, function(data){
+// 	console.log('Got it!', data);
+// });
+
+socket.on('newLocationMessage', function(message){
+	var li = jQuery('<li></li>');
+	var a = jQuery('<a target="_blank">My current location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e){
 	e.preventDefault();
+	var messageTextBox = jQuery('[name=message');
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message').val()
+		text: messageTextBox.val()
 	}, function(){
+		messageTextBox.val('');
+	});
+});
 
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(){
+	if(!navigator.geolocation){
+		return alert('Geolocation not supported by your browser.');
+	}
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+	navigator.geolocation.getCurrentPosition(function(position){
+		locationButton.removeAttr('disabled').text('Send location');
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	}, function(){
+		alert('Unable to fetch location.');
+		locationButton.removeAttr('disabled').text('Sending location');
 	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,25 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
+
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
 		var from = 'David';
 		var text = 'Test';
 		var res = generateMessage(from, text);
 		expect(res).toInclude({from, text});
+		expect(res.createdAt).toBeA('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'David';
+		var lat = 1;
+		var lon = 1;
+		var url = 'https://www.google.com/maps?q=1,1';
+		var res = generateLocationMessage(from, lat, lon);
+		expect(res).toInclude({from, url});
 		expect(res.createdAt).toBeA('number');
 	});
 });
